3a5940dd01aef13af33b2fb7fb8e06617acba17e
oussama <znagui.info@gmail.com>
2023-12-27 20:34:16 +0100

    traitement des reponse

--- model/answer.php
+++ model/answer.php
@@ -1,10 +1,45 @@
 <?php
-    class Answer{
-        private $id_answer;
-        private Question $question;
-        private $answer;
-        private $correction;
-        private $explication;
+class Answer
+{
+    private $id_answer;
+    private Question $question;
+    private $answer;
+    private $correction;
+    private $explication;
 
+    public function __construct($ida,$q,$answer,$correc,$exp){
+        $this->id_answer = $ida;
+        $this->question = $q;
+        $this->answer = $answer;
+        $this->correction = $correc;
+        $this->explication = $exp;
     }
-?>
+
+    public function __get($prop)
+    {
+        return $this->$prop;
+    }
+
+    public function __set($prop, $value)
+    {
+        $this->$prop = $value;
+    }
+
+    public static function getAnswer($question){
+        $idq = $question->idQ;
+        $sql = db::connexion()->query("SELECT * SELECT * FROM `answer` JOIN question WHERE answer.id_question = question.id_question AND question.id_question = $idq");
+        
+        
+
+
+        
+    }
+
+    
+
+
+    
+
+
+    
+}
